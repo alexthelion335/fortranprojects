18764f448795bae9205ba6b355820d570a3f01d1
Alex Kinch <alexthelion335@gmail.com>
2026-04-19 00:59:03 -0400

    AI Fixed Syntax and variable types

--- pi.for
+++ pi.for
@@ -1,19 +1,20 @@
 C Fortran program for calculating Pi
-C Using Gregory Leibniz Series
+C Using Chudnovsky Series
 C Alex Kinch, April 18, 2026
       program picalc
       implicit none
                   double precision pi, x
                   double precision fact
+                  external fact
                   integer q
 
                   pi = 0d0
                   x = 0d0
       do q = 0, 10, 1
                         x = x + ((((-1d0)**q) * fact(6*q) *
-             &    (545140134d0*q + 13591409d0)) /
-             &    (fact(3*q) * (fact(q))**3 *
-             &    (640320d0**(3*q + 1.5d0))))
+     1    (545140134d0*q + 13591409d0)) /
+     1    (fact(3*q) * (fact(q))**3 *
+     1    (640320d0**(3*q + 1.5d0))))
       end do
 C      do q = 0, 1000000000, 1
 C        pi4 = pi4 + sign * (1 / denom)
